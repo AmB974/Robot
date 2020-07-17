32c243eae6d150aa1dbbc15693234b60afe026aa
Ambre <ambriii.mussard@gmail.com>
2020-07-18 00:23:37 +0200

    DELETE projet

--- RobotMaven/src/main/java/robot/TropDePas.java
+++
@@ -1,9 +0,0 @@
-package robot;
-
-public class TropDePas extends Exception {
-    private static final long serialVersionUID = 1L;
-    public TropDePas() {
-        super("Trooop lond !!!! Utilise moins de Pas");
-    }
-
-}
